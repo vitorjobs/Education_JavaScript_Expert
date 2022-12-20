22a7937aa63203ebfdeec443fa1ad40dfaba5cb7
vitorjobs <vitorjobs@gmail.com>
2022-12-20 16:03:04 -0300

    Biblioteca sinion - MOCKS Utilização de Moks para validar resposta Implementação do getPlanet para buscar

--- aula02_stubs/src/service.js
+++ aula02_stubs/src/service.js
@@ -4,7 +4,15 @@ const https = require ('https')
 
 
 class Service {
-  // http://swapi.dev/api/planets/1/
+  /**
+   * COMENTÁRIO 01:
+   * @param {*} url 
+   * @returns response dados da requisição na API
+   * 
+   * CONSOME API: "https://swapi.dev/api/planets/1/"
+   * HTTP REQUEST: GET
+   * RETORNA: Dados da requisição 
+   */
 
   async mackeRequest(url) {
     return new Promise((resolve, reject) => {
@@ -13,8 +21,18 @@ class Service {
         response.on("error", reject)
       })
     })
-
   }
+
+  async getPlanets(url){
+    const result = await this.mackeRequest(url)
+
+    return {
+      name: result.name,
+      surfaceWater: result.surface_water,
+      appearedIn: result.films.length
+    }
+  }
+
 }
 
 module.exports = Service
--- aula02_stubs/src/service.test.js
+++ aula02_stubs/src/service.test.js
@@ -1,5 +1,6 @@
 const Service = require ('./service')
-const sinon = require ('sinon')
+const sinon   = require ('sinon')
+const {deepStrictEqual}  = require ('assert')
 const BASE_URL_1 = "https://swapi.dev/api/planets/1/"
 const BASE_URL_2 = "https://swapi.dev/api/planets/2/"
 const mocks = {
@@ -39,9 +40,29 @@ const mocks = {
 
     
       {
-        const response = await service.mackeRequest(BASE_URL_1)
-        console.log('response', response)
+        // const response = await service.mackeRequest(BASE_URL_1)
+        // console.log('response', response)
+
+        const expected = {
+          "name": "Tatooine",
+          "surfaceWater": "1",
+          appearedIn: 5
+        }
+        const results = await service.getPlanets(BASE_URL_1)
+        deepStrictEqual(results, expected)
       }
+    
+
+    {
+      const expected = {
+        "name": "Alderaan",
+        "surfaceWater":  "40",
+        appearedIn: 2
+      }
+      const results = await service.getPlanets(BASE_URL_2)
+      console.log(results, expected)
+      deepStrictEqual(results, expected)
+    }
 
 
 })()
